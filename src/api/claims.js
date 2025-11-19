84ba3956cc1c54ed7e3015f6ab091ea0f0660ed8
anishcm1982 <anishcm1982@gmail.com>
2025-11-19 23:19:35 +0530

    Update claims.js

--- src/api/claims.js
+++ src/api/claims.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8080/api/claims";
+const API = "http://13.201.202.44:8088/api/claims";
 
 export const uploadCsv = (formData) =>
   axios.post(`${API}/upload`, formData);
